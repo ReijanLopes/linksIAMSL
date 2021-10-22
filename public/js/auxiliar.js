5447903b32042334a413859f2ac8d353c77e69d0
Reijan <reijan.lj@gmail.com>
2021-10-22 11:20:47 -0300

    first commit

---
+++ public/js/auxiliar.js
@@ -0,0 +1,9 @@
+function  openUrl(url) {
+    
+    var URL = url;
+    URL.toString();
+
+
+    window.open(URL)
+}
+
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const axios = require('axios');
+// const { response } = require('express');
+
+
+const server = express();
+
+server.set('views', './src/views')
+server.set('view engine', 'ejs')
+
+server.use(express.static(__dirname + "/public"));
+
+
+
+const token = process.env.DATO_TOKEN;
+const PORT =  process.env.PORT;
+
+async function getAPIdata(){
+
+    let dados;
+    const hd = { headers:{'Authorization':  `Bearer ${token}`,}};
+
+    const Bd = JSON.stringify({
+        query: `{
+            allLinks {
+            id
+            nameLink
+            url
+            icon {
+                iconName
+                }
+            }
+                              
+            }`
+            });
+
+  
+            await axios.post('https://graphql.datocms.com/', Bd, hd).then(response => {dados = response.data.data })
+            return dados
+    }
+
+
+
+server.get('/', async (req, res) => {
+
+    const data = await getAPIdata()
+
+
+    res.render("home", {data})
+
+})
+
+
+server.listen(PORT || 3001, () => {
+    console.log(`Server Running on port ${PORT || 3001}`);
+})
+
